764f0ea238c44a88b8961444299dbbaf7c071b61
Thanga Srivalli <thanga.srivalli@ascendion.com>
2024-08-28 12:35:07 +0530

    Completed converting my code to follow coding standards

--- ProductsAPI/backend/ascexpressapp/routes/products.js
+++ ProductsAPI/backend/ascexpressapp/routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productController = require("../controllers/products");
+const productController = require("../controllers/Products");
 
 router.post('/', productController.createProduct);
 router.get('/', productController.getAllProducts);
